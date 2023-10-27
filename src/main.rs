7157dd7f31c86ebb3ed4c74cf809a858570d1c6a
malik672 <aremumalik01@gmail.com>
2023-10-27 18:44:15 +0100

    optimized

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+mod optimizor {
+    pub mod gas_tricks;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contract = "
+      string name
+    ";
+    
+    optimizor::gas_tricks::bytes32(contract);
+    optimizor::gas_tricks::openzepplin(contract);
+    optimizor::gas_tricks::safemath(contract);
+    optimizor::gas_tricks::token(contract);
+    optimizor::gas_tricks::uint_incur_overhead(contract)
+ 
 }
